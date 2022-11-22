861f07ae3021684efcd72873cf7a6ae252b140d4
Josh Ackland <joshackland98@gmail.com>
2022-11-22 17:33:44 +0800

    Challenge 6. Zigzag Conversion

---
+++ code/0006 Zigzag Conversion.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public string Convert(string s, int numRows) {
+        string[] rows = new string[numRows];
+
+        int row = 0;
+        bool directionDown = true;
+
+        foreach (char c in s){
+            rows[row] += c.ToString();
+
+            if (numRows > 1){
+                directionDown = row == 0 ? true : row == numRows - 1 ? false : directionDown;
+
+                row = directionDown ? row + 1 : row - 1;
+            }
+        }
+
+        string output = "";
+
+        foreach (string sRow in rows){
+            output += sRow;
+        }
+
+        return output;
+    }
+}
