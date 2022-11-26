ada5968c3ac512b5bd25e8e93736d978563402a3
Josh Ackland <joshackland@pm.me>
2022-11-26 17:44:45 +0800

    7 Reverse Integer

---
+++ code/0007 Reverse Integer.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int Reverse(int x) {     
+        bool isNegative = x < 0;
+
+        if (isNegative){
+            x = x * -1;
+        }
+
+        char[] charArray = x.ToString().ToCharArray();
+        Array.Reverse(charArray);
+        string strReverseX = new string(charArray);
+
+        int reverseNum = 0;
+        bool isNumeric = int.TryParse(strReverseX, out reverseNum);
+
+        if (!isNumeric) return 0;
+
+        return isNegative ? reverseNum * -1 : reverseNum;
+    }
+}
