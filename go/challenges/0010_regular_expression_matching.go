2a4b4b3fe3f7aadc9ca29461cec30964ca57caec
Josh Ackland <joshackland@pm.me>
2025-05-07 14:22:22 +0800

    go 10

---
+++ go/challenges/0010_regular_expression_matching.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func isMatch(s string, p string) bool {
+
+}
+
+func main0010() {
+	input := []int{2, 7, 11, 15}
+	expectedResult := 0
+
+	result := function(input)
+	fmt.Println("Result:", result)
+	fmt.Println("Expected:", expectedResult)
+}
